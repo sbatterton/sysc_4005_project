dda62eb4d588e48e2ddfed03bc65ccebb1afc0b2
Stephen Batterton <stephenbatterton@cmail.carleton.ca>
2023-02-10 15:48:06 -0500

    stuff

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
-#imports
+# imports
 from simulation import Inspector, Workstation
+import simpy
 
-if __name__ =="__main__":
+if __name__ == "__main__":
 
-	print("Enter nothing for default simulation values (1000, 28000)")
-	REPLICATIONS = int(input("Enter Replications: ") or "1000")
-	REPLICATION_DURATION = int(input("Enter time (sec): ") or "28000")
+    print("Enter nothing for default simulation values (1000, 28000)")
+    REPLICATIONS = int(input("Enter Replications: ") or "1000")
+    REPLICATION_DURATION = int(input("Enter time (sec): ") or "28000")
 
-	#import data from dat files
-	inspector_1_data = [float(x)*60 for x in open('data/servinsp1.dat').read().splitlines() if x]
-	inspector_2_data = [float(x)*60 for x in open('data/servinsp22.dat').read().splitlines() if x]
-	inspector_3_data = [float(x)*60 for x in open('data/servinsp23.dat').read().splitlines() if x]
+    # import data from dat files
+    inspector_1_data = [
+        float(x) * 60 for x in open("data/servinsp1.dat").read().splitlines() if x
+    ]
+    inspector_2_data = [
+        float(x) * 60 for x in open("data/servinsp22.dat").read().splitlines() if x
+    ]
+    inspector_3_data = [
+        float(x) * 60 for x in open("data/servinsp23.dat").read().splitlines() if x
+    ]
 
-	workstation_1_data = [float(x)*60 for x in open('data/ws1.dat').read().splitlines() if x]
-	workstation_2_data = [float(x)*60 for x in open('data/ws1.dat').read().splitlines() if x]
-	workstation_3_data = [float(x)*60 for x in open('data/ws1.dat').read().splitlines() if x]
+    workstation_1_data = [
+        float(x) * 60 for x in open("data/ws1.dat").read().splitlines() if x
+    ]
+    workstation_2_data = [
+        float(x) * 60 for x in open("data/ws1.dat").read().splitlines() if x
+    ]
+    workstation_3_data = [
+        float(x) * 60 for x in open("data/ws1.dat").read().splitlines() if x
+    ]
 
-	#   Execution loop
-    for iteration in range(1, REPLICATIONS+1):
+    #   Execution loop
+    for iteration in range(1, REPLICATIONS + 1):
 
         #   Environment
-        logger.info('Starting iteration ' + str(iteration))
-        main_env = simpy.Environment()
-        REPLICATION_VARIABLES = ReplicationVariables(logger)
+        env = simpy.Environment()
 
         # Class instantiations
-        
+        inspector_1 = Inspector(1, ["c1"], env, inspector_1_data)
+        inspector_2 = Inspector(2, ["c2", "c3"], env, inspector_2_data)
+
+        workstation_1 = Workstation(1, ["c1"], env, workstation_1_data)
+        workstation_2 = Workstation(2, ["c1", "c2"], env, workstation_2_data)
+        workstation_3 = Workstation(3, ["c1", "c3"], env, workstation_3_data)
 
         # Run simulation
         main_env.run(until=REPLICATION_DURATION)  # 'until' is simulation duration
-
-        #   Save iteration variables
-        # REPLICATION_OUTPUTS[iteration] = simulation_output.get_means()
-        SIMULATION_VARIABLES.append(REPLICATION_VARIABLES)
-        #   Simulation End
--- simulation.py
+++ simulation.py
@@ -1,27 +1,30 @@
-class Inspector(object):
+from simpy.resources import container
 
-    def __init__(self, n, c, env, simulation_variables):
-        self.n = n #number of the Inspector
-        self.c = c #will be just [c1] for inspector 1 and [c2, c3] for inspector 2
+
+class Inspector(object):
+    def __init__(self, n, c, env, data):
+        self.n = n  # number of the Inspector
+        self.c = c  # will be just [c1] for inspector 1 and [c2, c3] for inspector 2
         self.env = env
         self.action = env.process(self.run())
-        self.simulation_variables = simulation_variables
-    
+        self.data = data
+
     def run(self):
         while True:
             pass
 
-class Workstation(object):
 
-    def __init__(self, n, c, env, simulation_variables):
-        self.n = n #number of the Workstation
-        self.c = c #will be [c1], [c1, c2], [c1, c3]
+class Workstation(object):
+    def __init__(self, n, c, env, data):
+        self.n = n  # number of the Workstation
+        self.c = c  # will be [c1], [c1, c2], [c1, c3]
         self.env = env
-        self.simulation_variables = simulation_variables
+        self.data = data
+        self.buffer = {}
+        for n in c:
+            self.buffer[n] = container.Container(self.env, 2)
         self.action = env.process(self.run())
-        self.buffers = [[None]*2]*len(c)    #will make the buffers depending on c input, either 
-        #[[None, None]], or [[None, None], [None, None]], with indices corresponding to c list
-    
+
     def run(self):
         while True:
             pass
