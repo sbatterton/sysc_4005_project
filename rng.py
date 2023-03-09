62432050498678271da8fea8faade3626979f9f9
sbatterton <stephenbatterton@cmail.carleton.ca>
2023-03-08 21:49:11 -0500

    random number generator

---
+++ rng.py
@@ -0,0 +1,34 @@
+from random import randint
+
+#function for generating the random numbers, with n being the amount
+def combined_linear_congruential_generator(n):
+    #empty list which will hold the random numbers
+    randoms = []
+
+    #assigning values for m and a, from the suggestions from chapter 7, slide 12
+    m1 = 2147483563
+    a1 = 40014
+    m2 = 2147483399
+    a2 = 40692
+
+    #using python random library to generate the seed
+    x1 = randint(1, m1 - 1)
+    x2 = randint(1, m2 - 1)
+
+    for i in range(0, n):
+        #step 2
+        x1 = a1 * x1 % m1
+        x2 = a2 * x2 % m2
+
+        #step 3
+        x = (x1 - x2) % (m1 - 1)
+
+        #step 4
+        if x > 0:
+            randoms.append(x / m1)
+        else:
+            randoms.append((m1 - 1) / m1)
+    
+    return randoms
+
+print(combined_linear_congruential_generator(300))
